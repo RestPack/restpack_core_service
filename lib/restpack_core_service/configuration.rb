18da657fd08bb3630c90d5c2c0fdc55241fce798
Gavin Joyce <gavinjoyce@gmail.com>
2013-08-07 09:09:38 +0100

    moved config setup configuration.rb

--- lib/restpack_core_service/configuration.rb
+++ lib/restpack_core_service/configuration.rb
@@ -1,6 +1,13 @@
-
 module RestPack::Core::Service
   class Configuration < RestPack::Service::Configuration
+    attr_accessor :application_id
 
+  end
+
+  mattr_accessor :config
+  @@config = Configuration.new
+
+  def self.setup
+    yield @@config
   end
 end
