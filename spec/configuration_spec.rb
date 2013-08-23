ccb9f266a275f70fa15da2526c96edfb5705e4e4
Gavin Joyce <gavinjoyce@gmail.com>
2013-08-23 16:27:07 +0100

    removed unneeded spec

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
 
 describe RestPack::Core::Service::Configuration do
-  it "has a default table prefix" do
-    RestPack::Core::Service.config.db_table_prefix.should == "restpack_"
-  end
+
 end
