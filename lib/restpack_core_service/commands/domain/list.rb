b83b53a2636e2bc455bbba175fe984e1a4fa1fe5
Gavin Joyce <gavinjoyce@gmail.com>
2013-09-21 21:32:21 +0100

    added Domain::List command

---
+++ lib/restpack_core_service/commands/domain/list.rb
@@ -0,0 +1,19 @@
+module RestPack::Core::Service::Commands
+  module Domain
+    class List < RestPack::Service::Command
+      required do
+        integer :application_id
+      end
+
+      optional do
+        integer :page
+        integer :page_size
+        string :includes
+      end
+
+      def execute
+        Serializers::Domain.resource(inputs)
+      end
+    end
+  end
+end
