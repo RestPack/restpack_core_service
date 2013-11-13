d88d8bfe0e0ab227cc78abf152da80f97df6ed2b
Gavin Joyce <gavinjoyce@gmail.com>
2013-11-13 16:22:21 +0000

    enabling it_acs_as_create_command specs

--- spec/commands/application/create_spec.rb
+++ spec/commands/application/create_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
 describe Commands::Core::Application::Create do
-  # let(:response) { p "SS:#{subject}";subject.class.run(params) }
-
-  # it_acts_as_create_command(:core, :application)
-
-
+  it_acts_as_create_command(:core, :application)
 
   pending 'validate command inputs'
 end
