135f690051926c0223b43470f372cb73921096b5
Gavin Joyce <gavin.joyce@intercom.io>
2014-08-17 12:17:37 +0100

    use generic spec helper setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,25 +1,4 @@
-require 'rspec'
 require 'restpack_service/support/spec_helper'
 require 'restpack_core_service'
 
-config = YAML.load_file('./config/database.yml')
-ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || config['test'])
-
-migrations_path = File.dirname(__FILE__) + "/../db/migrate"
-migrations = ActiveRecord::Migrator.up(migrations_path)
-
-FactoryGirl.find_definitions
-
-DatabaseCleaner.strategy = :transaction
-
-RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-end
+setup
